fefef1da6e7471760e37ddfc32bcec17f5711f57
CodeStrate <56911705+CodeStrate@users.noreply.github.com>
2024-04-22 08:54:34 +0530

    Added new Models, some more results and requirements file.

--- upscale_images.py
+++ upscale_images.py
@@ -14,3 +14,30 @@ args = vars(ap.parse_args())
 model_name = args["model"].split(os.path.sep)[-1].split("_")[0].lower()
 model_scale = args["model"].split("_x")[-1]
 model_scale = int(model_scale[:model_scale.find(".")])
+
+print("[INFO] loading the SR model... {}".format(args["model"]))
+print("[INFO] SR Model Name : {}".format(model_name))
+print("[INFO] Model Scale : {}".format(model_scale))
+
+# init OCV's SR DNN (Deep Neural Network) object
+sr = cv2.dnn_superres.DnnSuperResImpl_create()
+sr.readModel(args["model"])
+sr.setModel(model_name, model_scale)
+
+image = cv2.imread(args["image"])
+print('[INFO] Width: {}, Height: {}'.format(image.shape[1], image.shape[0]))
+
+# use model and time it
+start_time = time.time()
+upscaled_image = sr.upsample(image)
+end_time = time.time()
+
+print('[INFO] the SR Model took {:.6f} seconds'.format(end_time - start_time))
+print('[INFO] Width: {}, Height: {}'.format(upscaled_image.shape[1], upscaled_image.shape[0]))
+
+cv2.imwrite(f'examples\\{model_name}_result.png', upscaled_image)
+
+cv2.imshow("Original : ", image)
+cv2.imshow("SR : ", upscaled_image)
+cv2.waitKey(0)
+
